30af9c85b1b69a2b9c8b56c77388157c0fe4e561
bbegum <45266034+bbegum@users.noreply.github.com>
2019-04-01 17:50:17 -0400

    Update HotelsPackagesPage.java

--- HotelsPackagesPage.java
+++ HotelsPackagesPage.java
@@ -8,6 +8,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HotelsPackagesPage extends BasePage {
+  
+  private By originField = By.xpath("//input[@id='package-origin-hp-package']");
+    private By destinationField = By.xpath("//input[@id='package-destination-hp-package']");
+    private By departureDate = By.xpath("//input[@id='package-departing-hp-package']");
+    private By returnDate = By.xpath("//input[@id='package-returning-hp-package']");
+    private By submitButton = By.xpath("//section[contains(@class,'package-form-fields')]//button[contains(@type,'submit')]");
+
+
+    public void setOriginField(String text){
+        setValue(originField, text );
+    }
+    public void setDestinationField(String text){
+        setValue(destinationField, text );
+    }
+    public void setDepartureDate(String text){
+        setValue(departureDate, text );
+    }
+    public void setReturnDate(String text){
+        setValue(returnDate, text );
+    }
+    public void clickSubmitButton(){
+        clickOn(submitButton);
+    }
+}
 
 
 
